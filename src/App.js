fcc703008d1628141f81b9bfd481e8ad28b356be
= <=>
2024-07-06 10:18:22 +0530

    Initialed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Home from './components/home/Home';
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world</h1>
+      <Home/>
+      <NewsComponent/>
+    </div>
+  );
+}
+
+export default App;
